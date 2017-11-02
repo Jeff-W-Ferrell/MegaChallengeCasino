d6f055bda5eac9878cfd64242cc9007546d5e270
Jeff Ferrell <jwferrell78@gmail.com>
2017-11-02 01:09:12 -0700

    Created a slot machine app with C# and Visual Studio
    Built specifically to practice breaking up application functionality and responsibilities into many small methods to satisfy business requirements.

---
+++ MegaChallengeCasino/CasinoChallenge.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CasinoChallenge.aspx.cs" Inherits="MegaChallengeCasino.CasinoChallenge" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-weight: normal;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Image ID="firstSlotMachineImage" runat="server" Height="150px" Width="150px" />
+        <asp:Image ID="secondSlotMachineImage" runat="server" Height="150px" Width="150px" />
+        <asp:Image ID="thirdSlotMachineImage" runat="server" Height="150px" Width="150px" />
+        <h3><span class="auto-style1">Your Bet:</span>&nbsp;
+            <asp:TextBox ID="yourBetTextBox" runat="server"></asp:TextBox>
+        </h3>
+        <p>
+            <asp:Button ID="pullLeverButton" runat="server" Text="Pull the Lever!" OnClick="pullLeverButton_Click" />
+        </p>
+        <p>
+            <asp:Label ID="winnerLoserLabel" runat="server"></asp:Label>
+        </p>
+        <p>
+            Player&#39;s Money:
+            <asp:Label ID="playersMoneyLabel" runat="server"></asp:Label>
+        </p>
+        <p>
+            1 Cherry - x2 Your Bet
+        <br/>
+            2 Cherry - x3 Your Bet
+        <br/>
+            3 Cherry - x4 Your Bet
+        <br/>
+        </p>
+        <p>
+            3 7&#39;s - Jackpot - x100 Your Bet</p>
+        <p>
+            HOWEVER</p>
+        <p>
+            If there&#39;s even one BAR, you win nothing</p>
+    </form>
+</body>
+</html>
---
+++ MegaChallengeCasino/CasinoChallenge.aspx.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MegaChallengeCasino
+{
+    public partial class CasinoChallenge : System.Web.UI.Page
+    {
+        int playerMoney = 0; int bet = 0; int winnings;
+        int firstImage = 0; int secondImage = 0; int thirdImage = 0;
+        String[] images = new string[12] { "Bar.png", "Bell.png", "Cherry.png", "Clover.png", "Diamond.png", "HorseShoe.png", "Lemon.png", "Orange.png", "Plum.png", "Seven.png", "Strawberry.png", "Watermellon.png" };
+        Random r = new Random();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            playerMoney = 100;
+            playersMoneyLabel.Text = String.Format("{0:C}", playerMoney);
+            randomThreeSymbols();
+            if (!Page.IsPostBack)
+                ViewState.Add("playerCash", playerMoney);
+        }
+
+        protected void pullLeverButton_Click(object sender, EventArgs e)
+        {
+            getBet();
+            randomThreeSymbols();
+            if (checkForBars())
+                checkForWinnings();
+            else youLose();
+            noBetEntered();
+        }
+
+        private bool getBet()
+        {
+            doesYourBetExist();
+            if (!int.TryParse(yourBetTextBox.Text.Trim(), out bet)) return false;
+            return true;
+        }
+
+        private bool doesYourBetExist()
+        {
+            if (yourBetTextBox.Text.Length < 0) return false;
+            return true;
+        }
+
+        private void randomThreeSymbols()
+        {
+            randomFirst();
+            randomSecond();
+            randomThird();
+        }
+
+        private int randomFirst()
+        {
+            firstImage = r.Next(0, 11);
+            firstSlotMachineImage.ImageUrl = images[firstImage];
+            return firstImage;
+        }
+
+        private int randomSecond()
+        {
+            secondImage = r.Next(0, 11);
+            secondSlotMachineImage.ImageUrl = images[secondImage];
+            return secondImage;
+        }
+
+        private int randomThird()
+        {
+            thirdImage = r.Next(0, 11);
+            thirdSlotMachineImage.ImageUrl = images[thirdImage];
+            return thirdImage;
+        }
+
+        private bool checkForBars()
+        {
+            if (firstImage == 0 || secondImage == 0 || thirdImage == 0) return false; 
+            return true;
+        }
+
+        private void checkForWinnings()
+        {
+            cherries();
+            sevens();
+            if (winnings > 0) youWin();
+            else youLose();
+        }
+
+        private int cherries()
+        { 
+            if (firstImage == 2 || secondImage == 2 || thirdImage == 2) winnings = bet * 2;
+            if ((firstImage == 2 && secondImage == 2) || (firstImage == 2 && thirdImage == 2) 
+                || (secondImage == 2 && thirdImage == 2))
+                winnings = bet * 3;
+            if (firstImage == 2 && secondImage == 2 && thirdImage == 2) winnings = bet * 4;
+            return winnings;
+        }
+
+        private int sevens()
+        {
+            if (firstImage == 9 && secondImage == 9 && thirdImage == 9) winnings = bet * 100;
+                return winnings;   
+        }
+
+        private void youLose()
+        { 
+            int playerMoney = (int)ViewState["playerCash"];
+            playerMoney -= bet;
+            playersMoneyLabel.Text = String.Format("{0:C}",playerMoney);
+            ViewState.Add("playerCash", playerMoney);
+            winnerLoserLabel.Text = String.Format("Sorry, you lost {0:C}.  Better luck next time.", bet);
+        }
+
+        private void youWin()
+        {
+            int playerMoney = (int)ViewState["playerCash"];
+            playerMoney += winnings;
+            playersMoneyLabel.Text = String.Format("{0:C}", playerMoney);
+            ViewState.Add("playerCash", playerMoney);
+            winnerLoserLabel.Text = String.Format("You bet {0:C} and won {1:C}", bet, winnings);
+        }
+
+        private void noBetEntered()
+        {
+            if (!getBet()) winnerLoserLabel.Text = "Place a bet and try your luck!";
+        }
+    }
+}
+
+     
+
+
